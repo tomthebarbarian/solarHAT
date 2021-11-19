6f6af2cb12061a329a7412f674a0ba1c2b2b3acf
Tom Zhang <tom.zhang989@gmail.com>
2021-11-19 13:43:09 -0500

    styles the html closer to a react component

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,48 +1,52 @@
-<head>
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-  integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-  crossorigin=""/>
-   <!-- Make sure you put this AFTER Leaflet's CSS -->
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-  crossorigin=""></script>
-</head>
-<body>
-  <div id="map" style=" height: 180px;"></div>
-</body>
+import React from "react";
+import leaflet from "leaflet"
 
-<footer>
-  <script>
-    const mymap = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer(
-      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
-      {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
-      }
-    ).addTo(mymap);
+const mymap = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer(
+  'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+  {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
+  }
+).addTo(mymap);
 
-    const marker = L.marker([51.5, -0.09]).addTo(mymap);
-    marker.bindPopup(
-      `
-      <b>Hello world!</b>
-      <br>I am a popup.
-      `
-    ).openPopup();
+const marker = L.marker([51.5, -0.09]).addTo(mymap);
+marker.bindPopup(
+  `
+  <b>Hello world!</b>
+  <br>I am a popup.
+  `
+).openPopup();
 
-    const popup = L.popup();
+const popup = L.popup();
 
-    const onMapClick = (e) => {
-      popup
-        .setLatLng(e.latlng)
-        .setContent(`You clicked the map at ${e.latlng}`)
-        .openOn(mymap);
-    }
+const onMapClick = (e) => {
+  popup
+    .setLatLng(e.latlng)
+    .setContent(`You clicked the map at ${e.latlng}`)
+    .openOn(mymap);
+}
 
-    mymap.on('click', onMapClick)
-  </script>
-</footer>
+mymap.on('click', onMapClick)
+ const Map = (props) => {
+  return (
+  // <head>
+  //   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+  //   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+  //   crossorigin=""/>
+  //   <!-- Make sure you put this AFTER Leaflet's CSS -->
+  //   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+  //   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+  //   crossorigin=""></script>
+  // </head>
+
+  <body>
+    <div id="map" style=" height: 180px;"></div>
+  </body>
+  )
+}
+export default Map
