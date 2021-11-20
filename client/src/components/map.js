6efe3cb07f1e628b40d3a0d2536b36b52556935f
Tom Zhang <tom.zhang989@gmail.com>
2021-11-19 20:04:20 -0500

    makes very basic use of useEffect to make a map

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,60 +1,86 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import L from "leaflet"
 
 
 
  const Map = (props) => {
-  return (
-  // <head>
-  //   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-  //   integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-  //   crossorigin=""/>
-  //   <!-- Make sure you put this AFTER Leaflet's CSS -->
-  //   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-  //   integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-  //   crossorigin=""></script>
-  // </head>
+  const [state, setState] = useState(
+    {
+      map: '',
+      marker:[]
+    }
+  )
+  const setMap = (map) => {
+    return setState(prev => {
+      return ({ ...prev, map })
+    })
+  }
 
+  const setMarker = (marker) => {
+    return setState(prev => {
+      return ({ ...prev, marker })
+    })
+  }
+
+   useEffect(() => {
+     setMap(L.map('map', {
+      center: [51.505, -0.09],
+      zoom: 13,
+      layers: [
+        L.tileLayer(
+          'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+          {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
+          }
+        )
+      ]
+    }))
+   },[])
+  // const mymap = L.map('map').setView([51.505, -0.09], 13);
+
+  // L.tileLayer(
+  //   'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+  //   {
+  //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //     maxZoom: 18,
+  //     id: 'mapbox/streets-v11',
+  //     tileSize: 512,
+  //     zoomOffset: -1,
+  //     accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
+  //   }
+  // ).addTo(mymap);
+  
+  // const marker = L.marker([51.5, -0.09]).addTo(mymap);
+  // marker.bindPopup(
+  //   `
+  //   <b>Hello world!</b>
+  //   <br>I am a popup.
+  //   `
+  // ).openPopup();
+  
+  // const popup = L.popup();
+  
+  // const onMapClick = (e) => {
+  //   popup
+  //     .setLatLng(e.latlng)
+  //     .setContent(`You clicked the map at ${e.latlng}`)
+  //     .openOn(mymap);
+  // }
+  
+  // mymap.on('click', onMapClick)
+
+  return (
   <div 
     id="map" 
     style={{height: "180px"}}
   >
-    this would be map
   </div>
-
   )
 }
 export default Map
 
-// const mymap = L.map('map').setView([51.505, -0.09], 13);
-
-// L.tileLayer(
-//   'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
-//   {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
-//   }
-// ).addTo(mymap);
-
-// const marker = L.marker([51.5, -0.09]).addTo(mymap);
-// marker.bindPopup(
-//   `
-//   <b>Hello world!</b>
-//   <br>I am a popup.
-//   `
-// ).openPopup();
-
-// const popup = L.popup();
-
-// const onMapClick = (e) => {
-//   popup
-//     .setLatLng(e.latlng)
-//     .setContent(`You clicked the map at ${e.latlng}`)
-//     .openOn(mymap);
-// }
-
-// mymap.on('click', onMapClick)
