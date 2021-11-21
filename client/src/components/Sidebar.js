3357e802dbd50de5b8f53fc1ddfbc805e7dc3114
Hamza Ghelle <hamzaghellegmail.com>
2021-11-21 18:17:32 -0500

    added component side bar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {Navigation} from 'react-minimal-side-navigation';
+import 'react-minimal-side-navigation/lib/ReactMinimalSideNavigation.css';
+import {BrowserRouter as Router, Route, useHistory} from 'react-router-dom'
+import Map from './map'
+
+
+const SideBar = props => {
+   
+const history = useHistory()
+  return (
+    <Router>
+    <>
+    <div className="sidebar">
+      <Navigation
+          
+          onSelect={({itemId}) => {
+            // push to the route  
+           history.push(itemId)
+           
+          }}
+          
+          items={[
+            {
+              title: 'Solar Map',
+              itemId: '/solarmap',
+              
+            },
+            {
+              title: 'My Sites',
+              itemId: '/sites',
+              
+              subNav: [
+                {
+                  title: 'Add Site',
+                  itemId: '/add_site',
+                },
+              
+              ],
+            },
+            {
+              title: 'Scoreboard',
+              itemId: '/scoreboard',
+              },
+              ]}
+        />
+       </div>   
+    </>
+    </Router>
+  );
+};
+
+export default SideBar
