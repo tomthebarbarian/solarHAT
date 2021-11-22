4a921b17e2b2d3b73bb63e0b8b8d470559f6ce23
Hamza Ghelle <hamzaghellegmail.com>
2021-11-22 15:26:52 -0500

    adding scoreboard component with axios request to db

--- client/src/components/Scoreboard.jsx
+++ client/src/components/Scoreboard.jsx
@@ -1,12 +1,18 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d70fbbf (adding scoreboard component with axios request to db)
 import {React,  Component} from "react";
 import axios from 'axios'
 import className from 'classnames'
 import './Scoreboard.css'
+<<<<<<< HEAD
 =======
 // import {React, axios, useState, Component, useEffect} from "react";
 // import {BrowserRouter as Router, Route} from 'react-router-dom'
 >>>>>>> dc54507 (no real changes)
+=======
+>>>>>>> d70fbbf (adding scoreboard component with axios request to db)
 
 
 
@@ -14,11 +20,15 @@ import './Scoreboard.css'
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d70fbbf (adding scoreboard component with axios request to db)
 export default class Scoreboard extends Component{
   state = {
     siteData: []
   }
    
+<<<<<<< HEAD
   
 
    siteInformation = function () {
@@ -63,24 +73,43 @@ export default class Scoreboard extends Component{
 //   const [state, setState] = useState({}) 
 
   
+=======
+>>>>>>> d70fbbf (adding scoreboard component with axios request to db)
   
 
-//   const scoresByHighestOrder = function () {
-//     Promise
-//     .all([
-//     axios.get('/api/sites')])
-//       .then((response) => {
-//         console.log(response[0].data)
-//         // this.setState(response.data)
-//       })
-//       .catch((err) => {
-//         console.log('Error while getting data', err)
-//       })
-//   }
-//   useEffect(scoresByHighestOrder)
+   siteInformation = function () {
+   
+    axios.get('/api/sites')
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
+        
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+  }
+
+  componentDidMount ()  {
+    this.siteInformation()
+  }
   
-//   return (
+  render() {
+  return (
+    <>
+    {console.log(this.state.siteData)}
+    <table>
+      {this.state.siteData.map((site) => {
+        
+   return( <tr>
+      <td>{site.name}</td>
+      <td>{site.province}</td>
+      <td>{site.usage_kWh}</td>
+    </tr>
+   )
+      })}
     
+<<<<<<< HEAD
 //     <table>
 //     <tr>
 //       <td>Centro comercial Moctezuma</td>
@@ -93,3 +122,9 @@ export default class Scoreboard extends Component{
 // }
 
 >>>>>>> dc54507 (no real changes)
+=======
+  </table>
+    </>
+  )}
+  }
+>>>>>>> d70fbbf (adding scoreboard component with axios request to db)
