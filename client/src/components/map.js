7636151f99a897607faacc3c742c7ba8b05966ee
Tom Zhang <tom.zhang989@gmail.com>
2021-11-22 10:58:10 -0500

    loads some markers into the intitial rendering of map

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect } from "react";
-import L from "leaflet"
+import L, {addLayer} from "leaflet"
 // Need to find leaflet.css and import into webpack
 // import "node_modules/leaflet/dist/leaflet.css"
 // import "./leaflet.css"
@@ -18,32 +18,50 @@ import "./map.scss"
       return ({ ...prev, map })
     })
   }
+  const marker = L.marker([45.521020, -73.614750])
+  
 
+  const addMarker = (marker) => {
+    if (state.map !== '') {
+      marker.addTo(state.map)
+    }
+  }
 
   useEffect(() => {
     setMap(L.map('map', 
       {
       center: [45.489934, -73.566805],
       zoom: 13,
-      layers: [ L.tileLayer(
+      layers: [ 
+        L.tileLayer(
         'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
-        {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox/streets-v11',
-          tileSize: 512,
-          zoomOffset: -1,
-          accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
-        }
-        )]
-      }
-    ))
-    // Add these in
-  //   addMarker = (marker) => {
-  //     state.map
-  //   }
-  //   const marker = L.marker([51.5, -0.09]).addTo(state.map);
-    
+          {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoidG9tdGhlYmFyYmFyaWFuIiwiYSI6ImNqZmZ4Z2ZvczJhaXgzM3BheHR3Nml2OGYifQ.jq0Tt-4aD5EpAaQ8ihykLw'
+          }
+        ),
+        L.marker([45.521020, -73.614750]),
+        L.marker([45.489934, -73.566805])
+      ]
+      })
+    )
+
+    // addMarker(marker)
+
+    if (state.map) {
+      marker.addTo(state.map)
+    }
+
+    const addLayer = () => {
+      L.layerGroup.addTo(state.map)
+    }
+
+
+
   //   marker.bindPopup(
   //     `
   //     <b>Hello world!</b>
