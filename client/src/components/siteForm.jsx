4263b629384a10b739f4e58224eeae717b0adaf7
Hamza Ghelle <hamzaghellegmail.com>
2021-11-22 19:40:00 -0500

    adding axios post request to the form

--- client/src/components/siteForm.jsx
+++ client/src/components/siteForm.jsx
@@ -1,71 +1,69 @@
-import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 978abec (adding site form component to file with map integration)
+import {React, Component} from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
 import Map from "./map";
 import classNames from "classnames";
 import './siteForm.css'
-<<<<<<< HEAD
-=======
-import {BrowserRouter as Router, Route} from 'react-router-dom'
->>>>>>> dc54507 (no real changes)
-=======
->>>>>>> 978abec (adding site form component to file with map integration)
+import axios from 'axios'
 
 
+export default class SiteForm extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+    name: '',
+    longitude: '',
+    latitude: '',
+    province: '',
+    usage_kWh: ''
+  }
+  }
+  
+  changeHandler = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
 
-function SiteForm() {
+  submitHandler = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+    axios.post('/api/sites', this.state)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render(){
+    const {name, longitude, latitude, usage_kWh}= this.state
   return (
     <Router>
     <>
     <h1>Add Site</h1>
-<<<<<<< HEAD
-<<<<<<< HEAD
     
-    <form>
+    <form onSubmit={this.submitHandler}>
       <div>
-        <input type='text' name='name' placeholder='Site name' />
+        <input type='text' name='name' placeholder='Site name' value={name} onChange={this.changeHandler} />
       </div>
       <div>
-        <input type='text' name='longitude' placeholder='longitude' />
-        <input type='text' name='latitude' placeholder='latitude' />
+        <input type='text' name='longitude' placeholder='longitude' value={longitude} onChange={this.changeHandler}/>
+        <input type='text' name='latitude' placeholder='latitude' value={latitude} onChange={this.changeHandler}/>
       </div>
+      {/* <div>
+        <input type='text' name='size' placeholder='Panel Size' value={size} onChange={this.changeHandler}/>
+      </div> */}
       <div>
-        <input type='text' name='size' placeholder='Panel Size' />
+        <input type='text' name='usage_kWh' placeholder='Consumption' value={usage_kWh} onChange={this.changeHandler}/>
       </div>
-      <input type='text' name='consumption' placeholder='Consumption' />
+      <button type="submit">Submit</button>
+      
+
     </form>
     
     <Map />
-=======
-=======
-    
->>>>>>> 978abec (adding site form component to file with map integration)
-    <form>
-      <div>
-        <input type='text' name='name' placeholder='Site name' />
-      </div>
-      <div>
-        <input type='text' name='longitude' placeholder='longitude' />
-        <input type='text' name='latitude' placeholder='latitude' />
-      </div>
-      <div>
-        <input type='text' name='size' placeholder='Panel Size' />
-      </div>
-      <input type='text' name='consumption' placeholder='Consumption' />
-    </form>
-<<<<<<< HEAD
->>>>>>> dc54507 (no real changes)
-=======
-    
-    <Map />
->>>>>>> 978abec (adding site form component to file with map integration)
     </>
     </Router>
     
-  );
-}
-
-export default SiteForm
+  )}
+  }
