859a33106fa89fed2a8c33a6b723d44db912b0bb
Tom Zhang <tom.zhang989@gmail.com>
2021-11-23 17:01:04 -0500

    adds the avg monthly production multiplied by size of the installation

--- client/src/components/Analytics/Analytics.jsx
+++ client/src/components/Analytics/Analytics.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
 
-import Map from '../Map'
-import BarchartCompare from '../BarchartCompare';
+import axios from 'axios'
 
-export default function Analytics() {
+import ProductionBar from './ProductionBar';
+
+
+
+
+export default function Analytics(props) {
+  const [state, setState] = useState(
+    {
+      monthData: [36,79,134,148,129,125,117,105,82,51,35,24],
+      provinceModel: {
+        "_id":"619bd8f50e7e193e9fd00d0e",
+        "ON":{
+          "pv_monthly_avg":[66,92,109,115,119,124,125,118,104,86,56,52],
+          "cost_cents_avg":13}
+      },
+      site: {
+        "_id":1,
+        "name":"aj",
+        "coord":[45.5462,-73.36564],
+        "province":"ON",
+        "usage_kWh":12.5,
+        "size_kW":10.5}
+    }
+  );
+  
+  const monthData = state.monthData
+
+  const setMonthData = (data) => {
+    
+  }
+
+  const siteData = state.site
+
+  const producedata = monthData.map(elem => elem * siteData.size_kW)
+
+  // Promise.all([
+  //   axios.get('/api/sites')]
+  // )
+
   return (
     <>
-      <Map />
-      <BarchartCompare />
-      <BarchartCompare />
-      <BarchartCompare/>
-
+      <ProductionBar monthProduction= {producedata}/>
     </>
   );
 }
--- client/src/components/Analytics/ProductionBar.jsx
+++ client/src/components/Analytics/ProductionBar.jsx
@@ -1,1 +1,63 @@
 import Chart from 'chart.js/auto'
+import "./ProductionBar.scss"
+import React, { useEffect, useState, useRef } from 'react';
+
+const chartConfig = {
+  type: 'bar',
+  data: {
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+      datasets: [{
+          label: '# kWH produced',
+          data: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+  }
+};
+
+// Here's the start of barchart compare
+// Should generate a barchart for all avg kw production for that number of 
+// months and year
+const ProductionBar = (props) => {
+  const chartContainer = useRef(null);
+  const [state, setState] = useState(
+    {
+      graph: {},
+    }
+  );
+
+  const setGraph = (graph) => {
+    return setState(prev => {
+      return ({ ...prev, graph })
+    })
+  }
+
+  chartConfig.data.datasets[0].data = props.monthProduction
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chart(chartContainer.current, chartConfig);
+      setGraph(newChartInstance);
+    }
+  }, [chartContainer]);
+
+  return (
+    <div>
+      <canvas ref={chartContainer} class ='comparebar'/>
+    </div>
+  );
+};
+
+export default ProductionBar;
