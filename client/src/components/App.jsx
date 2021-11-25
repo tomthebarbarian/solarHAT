11cdd82313003b15fa67e1648387fa10a14e6d62
AJ <aj@smartnvm.com>
2021-11-25 14:30:03 -0500

    - fix analytics to use global state - fix db_seed dataModel data structure to simplify provinceModel

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -164,7 +164,7 @@ export default function App() {
                
           <section>
               {/* {nav.editMap && <EditSite />} */}
-              {nav.analytics &&  <Analytics />}
+              {nav.analytics &&  <Analytics state={state} setState={setState} />}
               {nav.addSite &&  <EditSite state={state} setState={setState}/>}
               {nav.addSite &&  <AddForm/>}
               {nav.leaderBoard &&  <Scoreboard/>}
