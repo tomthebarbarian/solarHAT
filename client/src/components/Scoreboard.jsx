335ea175476f68717f55618fa5d71e9645ec9485
Hamza Ghelle <hamzaghellegmail.com>
2021-11-25 21:22:49 -0500

    adding sort feature

--- client/src/components/Scoreboard.jsx
+++ client/src/components/Scoreboard.jsx
@@ -10,14 +10,15 @@ import './Scoreboard.css'
 
 export default class Scoreboard extends Component{
   state = {
-    siteData: []
+    siteData: [],
+    option: ''
   }
    
   
 
-   siteInformation = function () {
+   siteInformation = function (option) {
    
-    axios.get('/api/sites')
+    axios.get(`/api/sites/${option}`)
       .then((response) => {
         // console.log(response.data)
         this.setState({siteData: response.data })
@@ -29,30 +30,97 @@ export default class Scoreboard extends Component{
   }
 
   componentDidMount ()  {
-    this.siteInformation()
+    this.siteInformation(this.state.option)
+
+  }
+  
+  changeHandler = (e) => {
+    this.setState({option: e.target.name}, ()=> {
+      if(this.state.option === 'usage') {
+      axios.get('/api/sites/usage')
+      .then((response) => {
+        console.log(response.data)
+        this.setState({siteData: response.data })
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+    }
+
+    if(this.state.option === 'production') {
+      axios.get('/api/sites/production')
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
+        
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+    }
+    if(this.state.option === 'cost') {
+      axios.get('/api/sites/cost')
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
+        
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+    }
+    })
+
+    
+    
+
   }
   
   render() {
+    
   return (
     <>
     {/* {console.log(this.state.siteData)} */}
+    <div className='filter'>
+      <div>
+        <label for='usage'>Consumption</label>
+        <input type='radio' id='usage'  name='usage' onChange={this.changeHandler} checked={this.state.option === 'usage'}/>
+      </div>
+      <div>
+        <label for='production'>Production</label>
+        <input type='radio' id='production'  name='production' onChange={this.changeHandler}  checked={this.state.option === 'production'} />
+      </div>
+      <div>
+        <label for='cost'>Cost</label>
+        <input type='radio' id='cost' name='cost'  onChange={this.changeHandler} checked={this.state.option === 'cost'} />
+      </div>
+      
+    </div>
     <table class="table">
   <thead>
     <tr>
-      <th scope="col">Ranking</th>
+      <th scope="col">#</th>
       <th scope="col">Name</th>
-      <th scope="col">Usage</th>
+      <th scope="col">Consumption</th>
+      <th scope="col">Production</th>
+      <th scope="col">Cost</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+    {this.state.siteData.map((site) => {
+      
+      return(<>
+      <tr>
+        <th scope='row'>{}</th>
+        <td>{site.name}</td>
+        <td>{site.usage_kWh}</td>
+        <td>{site.production}</td>
+        <td>{site.cost}</td>
+      </tr>
+      </>)
+    })}
     </tbody>
-    </table>  
+    </table>
     </>
   )}
   }
