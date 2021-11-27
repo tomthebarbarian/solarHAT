fd75cd67f9f4681b10b742518b7c9fa93794e51d
AJ <aj@smartnvm.com>
2021-11-27 15:02:56 -0500

    - add edit site starter code for Hamza

--- client/src/components/Edit/EditSite.jsx
+++ client/src/components/Edit/EditSite.jsx
@@ -1,322 +1,129 @@
-import React from 'react';
-import { Button, Modal, Form, Row, Col, InputGroup } from 'react-bootstrap';
-import classNames from 'classnames';
-import { useState } from 'react';
-
-import '../custom.scss';
-
-import axios from 'axios';
-
-import Geocode from 'react-geocode';
-const DECIMALS =  6
-Geocode.setApiKey('AIzaSyCcKsKVOs-uzI8Ri0xtVmP-Mi9NNsFkj_c');
-Geocode.setLanguage('en');
-Geocode.setRegion('ca');
-
-export default function EditSite(props) {
-  const { onClick, apiLogout, state, setState } = props;
-
-  const [user, setUser] = useState({});
-  const [code, setCode] = useState(false);
-
-  const [mode, setMode] = useState(false);
-
-  const [validated, setValidated] = useState(false);
-
-  const [site, setSite] = useState({
-    name: '',
-    lat: '',
-    long: '',
-    usage_kWh: '',
-    size_kW: '',
-    province: '',
-    address: '',
-    // zip: '',
-  });
-
-
-  // const [dummy , setDummy] = useState()
-
-
-  const changeHandler = (e) => {
-    setSite((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const submit = () => {
-    const newSite = { ...site };
-    const coord = [Number(site.lat), Number(site.long)];
-    delete newSite.lat;
-    delete newSite.long;
-    newSite.coord = coord;
-    setSite((prev) => ({ ...newSite }));
-
-    const newSites = [...state.sites, newSite];
-
-    setState((prev) => ({ ...prev, sites: newSites }));
-
-    axios
-      .post('/api/sites', newSite)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    clear();
-  };
+import {React,  Component} from "react";
+import axios from 'axios'
+import className from 'classnames'
+import '../Scoreboard.css'
 
 
 
-const x = document.getElementById("demo");
 
-function getLocation() {
-  console.log('button clicked')
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+
+
+export default class EdiSite extends Component{
+  
+  
+  
+  state = {
+    siteData: [],
+    option: ''
   }
-}
-
-function showPosition(position) {
+   
   
-const lat = position.coords.latitude
-const long =  position.coords.longitude
-  setSite( prev => ({...prev, lat, long}))
- 
-}
 
-
-  const fetchLatLong = () => {
-    console.log('-----------------------')
-    console.log(site.address)
-    Geocode.fromAddress(site.address)
-      .then((res) => {
-        console.log(res.results[0]);
-        const lat = res.results[0].geometry.location.lat.toFixed(DECIMALS);
-        const long = res.results[0].geometry.location.lng.toFixed(DECIMALS);
-        const addrComp = res.results[0].address_components
+   siteInformation = function (option) {
+   
+    axios.get(`/api/sites/${option}`)
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
         
-        
-        let province = ''
-        for (const e of addrComp) {
-
-          if (Object.keys({...state.model[0]}).includes(e.short_name)) {
-            province =  e.short_name
-            break
-          }
-
-        }
-        console.log({province})
-        setSite( prev => ({...prev, lat, long, province}))
-        setState(prev => ({...prev, marker:{lat: lat, lng: long}}))
-        
-        
-        // setDummy(state.map.L.marker([state.marker.lat,state.marker.lng]))
-        // state.map.addLayer(dummy)
-
-
-
-
-        console.log(site)
-        // const province = res.results[0].address_components[4].short_name;
-        // setState({ latitude: lat, longitude: lng, province: province });
-        // console.log('state with correct coord', this.state);
       })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+  }
 
-  const clear = () => {
-    // setValidated(false)
-    setSite((prev) => ({
-      ...{},
-      name: '',
-      lat: '',
-      long: '',
-      usage_kWh: '',
-      size_kW: '',
-      province: '',
-      address: '',
-      zip: '',
-    }));
+  componentDidMount ()  {
+    this.siteInformation(this.state.option)
 
-    setValidated((prev) => false);
-
-    // state.map.removeLayer(dummy)
-  };
+  }
   
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    event.preventDefault();
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
+  changeHandler = (e) => {
+    this.setState({option: e.target.name}, ()=> {
+      if(this.state.option === 'usage') {
+      axios.get('/api/sites/usage')
+      .then((response) => {
+        console.log(response.data)
+        this.setState({siteData: response.data })
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
     }
 
-    setValidated((prev) => true);
-
-    if (form.checkValidity()) {
-      submit();
-      setValidated((prev) => false);
-    }
-  };
-
-  const { name, lat, long, usage_kWh, size_kW, address, province} = site;
-
-  return (
-    <main className='cols'>
-      <h1> Edit Site</h1>
-      <Form
-        noValidate
-        validated={validated}
-        onSubmit={handleSubmit}
-        className=''
-      >
-        <Row className='mb-3'>
-          <Form.Group as={Col} md='6' controlId='validationCustom01'>
-            <Form.Label>Site Name</Form.Label>  
-            
-            <Form.Control
-              required
-              type='text'
-              placeholder='Example Site'
-              name={'name'}
-              value={name}
-              onChange={changeHandler}
-            />
-            {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
-          </Form.Group>
-
-          <Form.Group as={Col} md='1' className='icon' controlId='validationCustom02'>
-            <div id = "demo" >
-            <button > 
-              <img src='./geoicon.png' alt='logo' height='32' onClick={() => getLocation()} title="Get current location [Lat, Long]"/>
-             </button>
-            </div> 
-          
-            </Form.Group>
+    if(this.state.option === 'production') {
+      axios.get('/api/sites/production')
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
         
-        </Row>
-        <Row className='mb-3'>
-          <Form.Group as={Col} md='4' controlId='validationCustom02'>
-            <Form.Label>Latitude</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder='45.5045'
-              name={'lat'}
-              value={lat}
-              onChange={changeHandler}
-            />
-          {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+    }
+    if(this.state.option === 'cost') {
+      axios.get('/api/sites/cost')
+      .then((response) => {
+        // console.log(response.data)
+        this.setState({siteData: response.data })
+        
+      })
+      .catch((err) => {
+        console.log('Error while getting data', err)
+      })
+    }
+    })
 
-          </Form.Group>
-            <Form.Group as={Col} md='4' controlId='validationCustom02'>
-            <Form.Label>Longitude</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder='-75.6554'
-              onChange={changeHandler}
-              name={'long'}
-              value={long}
-            />
-          </Form.Group> 
-            {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
-           
-        </Row>
+    
+    
 
-        <Row className='mb-3'>
-          <Form.Group as={Col} md='6' controlId='validationCustom01'>
-            <Form.Label>Avg. Consumption [ kWh / Yr ]</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder=''
-              name={'usage_kWh'}
-              value={usage_kWh}
-              onChange={changeHandler}
-            />
-            {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
-          </Form.Group>
-          <Form.Group as={Col} md='4' controlId='validationCustom02'>
-            <Form.Label>PV System Size [ kW ]</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder=''
-              name={'size_kW'}
-              value={size_kW}
-              onChange={changeHandler}
-            />
-            {/* <Form.Control.Feedback>Looks good!</Form.Control.Feedback> */}
-          </Form.Group>         
-        </Row>
-        <Row className='mb-3'>
-       
-        <Form.Group as={Col} md='1' controlId='validationCustom03'>
-        <Form.Label>Get</Form.Label>
-
-           <button > 
-              <img src='./geo.png' alt='logo' height='32' onClick={fetchLatLong}/>
-          </button>
-          </Form.Group>
+  }
+  
+  render() {
+    
+  return (
+    <>
+    {/* {console.log(this.state.siteData)} */}
+    <div className='filter'>
+      <div>
+        <label for='usage'>Consumption</label>
+        <input type='radio' id='usage'  name='usage' onChange={this.changeHandler} checked={this.state.option === 'usage'}/>
+      </div>
+      <div>
+        <label for='production'>Production</label>
+        <input type='radio' id='production'  name='production' onChange={this.changeHandler}  checked={this.state.option === 'production'} />
+      </div>
+      <div>
+        <label for='cost'>Cost</label>
+        <input type='radio' id='cost' name='cost'  onChange={this.changeHandler} checked={this.state.option === 'cost'} />
+      </div>
       
-          <Form.Group as={Col} md='6' controlId='validationCustom03'>
-            <Form.Label>address</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder='Ottawa'
-              name={'address'}
-              value={address}
-              onChange={changeHandler}
-            />
-            <Form.Control.Feedback type='invalid'>
-              Please provide a valid address.
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group as={Col} md='3' controlId='validationCustom04'>
-            <Form.Label>Province</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder='ON'
-              name={'province'}
-              value={province}
-              onChange={changeHandler}
-            />
-             
-            <Form.Control.Feedback type='invalid'>
-              Please provide a valid state.
-            </Form.Control.Feedback>
-          </Form.Group>
-          {/* <Form.Group as={Col} md='3' controlId='validationCustom05'>
-            <Form.Label>Zip</Form.Label>
-            <Form.Control
-              required
-              type='text'
-              placeholder='A0A 0Z0'
-              name={'zip'}
-              value={zip}
-              onChange={changeHandler}
-            />
-            <Form.Control.Feedback type='invalid'>
-              Please provide a valid zip.
-            </Form.Control.Feedback>
-          </Form.Group> */}
-        </Row>
-        <Button type='submit' variant='outline-success'>
-          Submit
-        </Button>
-        <span> </span>
-        <Button type='button' variant='outline-dark' onClick={clear}>
-          Clear
-        </Button>
-      </Form>
-    </main>
-  );
-}
+    </div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Consumption</th>
+      <th scope="col">Production</th>
+      <th scope="col">Cost</th>
+    </tr>
+  </thead>
+  <tbody>
+    {this.state.siteData.map((site) => {
+      
+      return(<>
+      <tr>
+        <th scope='row'>{}</th>
+        <td>{site.name}</td>
+        <td>{site.usage_kWh}</td>
+        <td>{site.production}</td>
+        <td>{site.cost}</td>
+      </tr>
+      </>)
+    })}
+    </tbody>
+    </table>
+    </>
+  )}
+  }
