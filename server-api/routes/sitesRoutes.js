5c6712108e259f8c88004ac97630c122ea2316d1
AJ <aj@smartnvm.com>
2021-11-28 07:27:49 -0500

    - fixing Hamza broken code

--- server-api/routes/sitesRoutes.js
+++ server-api/routes/sitesRoutes.js
@@ -58,18 +58,38 @@ module.exports = (router, dbo) => {
     dbConn
       .collection("sites")
       .find()
-      .sort({usage_kWh: 1})
+      .sort({ usage_kWh: 1 })
       .toArray(function (err, result) {
         if (err) throw err;
         res.json(result);
       })
   })
-  router.get('/sites/cost', (req, res) => {
+
+  router.post("/sites/edit/:id", (req, res) => {
+    const site = req.body
+    console.log('--------POST: [param]:', req.params.id)
+    console.log('--------POST: [site]:', site)
+
+    delete site._id
+    console.log('--------POST: [site]:', site)
+
+    res.json(site)
+    return
+    const dbConn = dbo.getDb();
+    dbConn
+      .collection("sites")
+      .update({ "_id": ObjectId(req.parms.id) }, { $set: site }, { upsert: false })
+    // .update(ObjectId(req.params.id), { $set: site })
+  });
+
+
+
+  router.get('/sites/usage', (req, res) => {
     const dbConn = dbo.getDb();
     dbConn
       .collection("sites")
       .find()
-      .sort({cost: 1})
+      .sort({ cost: 1 })
       .toArray(function (err, result) {
         console.log("Result from server", result)
         if (err) throw err;
@@ -81,7 +101,7 @@ module.exports = (router, dbo) => {
     dbConn
       .collection("sites")
       .find()
-      .sort({production: -1 })
+      .sort({ production: -1 })
       .toArray(function (err, result) {
         if (err) throw err;
         res.json(result);
@@ -93,24 +113,26 @@ module.exports = (router, dbo) => {
 
     dbConn
       .collection("sites")
-      .update({_id: ObjectId(req.params.id)}, {$set:{
-        name: req.body.name,
-        usage_kWh: req.body.usage_kWh,
-        size_kW: req.body.size_kW,
-        province: req.body.province,
-        address: req.body.address,
-        coord: req.body.coord
-      } }, function(err, result){
+      .update({ _id: ObjectId(req.params.id) }, {
+        $set: {
+          name: req.body.name,
+          usage_kWh: req.body.usage_kWh,
+          size_kW: req.body.size_kW,
+          province: req.body.province,
+          address: req.body.address,
+          coord: req.body.coord
+        }
+      }, function (err, result) {
         res.send(result)
       })
   });
 
-  router.post("/sites/delete/:id", (req, res)=> {
+  router.post("/sites/delete/:id", (req, res) => {
     const dbConn = dbo.getDb();
 
     dbConn
       .collection("sites")
-      .deleteOne({_id: ObjectId(req.params.id)},function(err, result){
+      .deleteOne({ _id: ObjectId(req.params.id) }, function (err, result) {
         res.send(result)
       })
   })
