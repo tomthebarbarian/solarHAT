dd955085a7627979f166a743b1c4bbeda4e69ceb
Hamza Ghelle <hamzaghellegmail.com>
2021-11-28 22:04:17 -0500

    fixed user session id to grab user id

--- server-api/routes/userRoutes.js
+++ server-api/routes/userRoutes.js
@@ -109,7 +109,9 @@ module.exports = (router, dbo) => {
 
 
           //create session cookie
-          req.session.user_id = user.id;
+          console.log("regiser user id", user._id)
+          req.session.user_id = user._id;
+          console.log("from register", req.session.user_id)
           // dbConn.collection("users").update(
           //   {email,},
           //   {$set: {_id: }}
